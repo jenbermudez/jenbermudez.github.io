0d51892876973731d9f0b419e3ff17d3d4a11209
jenbermudez <jenbermudez@gmail.com>
2017-04-25 20:09:56 -0700

    draft

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+var $ = require('jquery.scrollto')
 // navbar responsiveness
 window.navToggle = function (x) {
   var menu = document.getElementsByClassName("nav__links")[0];
   menu.classList.toggle("responsive");
   x.classList.toggle("open");
+}
+
+window.smoothScroll = function (selector, x) {
+  $(window).scrollTo(selector, 800);
+  if(x) {
+    var hamburger = document.getElementsByClassName("hamburger")[0];
+    window.navToggle(hamburger);
+  }
 }
